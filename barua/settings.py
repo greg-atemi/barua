06d7d257e7498fc91893c193a94ad5860a468cfe
gregatemi <greg.atemi@systechafrica.com>
2024-08-11 01:38:29 +0300

    Added remote Postgres configuration

--- barua/settings.py
+++ barua/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'barua.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'barua',
+        'USER': 'barua',
+        'PASSWORD': 'ZPixfE78IzN01jGtkfct',
+        'HOST': '5.182.5.149',
+        'PORT': '5432',
     }
 }
 
